f8311335fdc530ff25ea33af175fa5e7bc843b09
Aidan Crane <aidancrane78@gmail.com>
2021-10-18 20:35:37 +0100

    Some work on the command line.
    It looks really good now I think!

--- public/src/CommandLine.js
+++ public/src/CommandLine.js
@@ -15,21 +15,38 @@ class CommandLine extends React.Component {
     };
     this.line = React.createRef();
     this.command = React.createRef();
+
+    // input box carat location monitor.
+    this.commandLengthInEM = 0;
   }
 
 
-  resize() {
+  //https://stackoverflow.com/a/58705306
+   getTextWidth(text, font) {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    context.font = font || getComputedStyle(document.body).font;
+    return context.measureText(text).width;
+  }
+
+  setCaretPosition() {
     var textContent = this.line.current.children[0];
-    console.log(textContent.width);
-    textContent.style.width = 24;
-    console.log("The part above this doesn't work");
+    var newInputLength = this.getTextWidth(textContent.value);
+    console.log(newInputLength); // <-- String length
+
+    //console.log(this.command.current.offsetWidth); // <- Width of input box
+
+    this.setState({commandLengthInEM: newInputLength});
+    //textContent.style.width = 24;
+    //console.log("The part above this doesn't work");
   }
 
   // Set the state every time there is a change to the commandline
   changeOccuring = (e) => {
     this.setState({command: e.target.value});
     // console.log(this.state);
-    this.resize();
+    e.target.width = null;
+    this.setCaretPosition();
   }
 
   render() {
@@ -37,7 +54,7 @@ class CommandLine extends React.Component {
       <header className="CommandLineBackground"></header>
       <div className="CommandLineText">
         <div ref={this.line} className="CommandLineHeadLine">
-          aidan@dark.infinityflame.co.uk ¬ /var/www/subdomains/dark ><input ref={this.command} onChange={(e) => {
+          aidan@dark.infinityflame.co.uk ¬ /var/www/subdomains/dark ><input autoFocus={true} ref={this.command} style={{ width: this.state.commandLengthInEM, maxWidth: "95vw" }} onChange={(e) => {
         this.changeOccuring(e)
       }} className="CommandLineInput" spellCheck="false" type="text"/>
           <span className="blink">█</span>
