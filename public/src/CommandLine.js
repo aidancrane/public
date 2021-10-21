7b04bf9001bff3f5269970a094785dd3a931b454
Aidan Crane <aidancrane78@gmail.com>
2021-10-21 23:14:28 +0100

    Update CommandLine.js
    Added clickable screen and selection stuff.

--- public/src/CommandLine.js
+++ public/src/CommandLine.js
@@ -18,27 +18,35 @@ class CommandLine extends React.Component {
 
     // input box carat location monitor.
     this.commandLengthInEM = 0;
+
+    this.focusCommandLine = this.focusCommandLine.bind(this);
   }
 
-
-  //https://stackoverflow.com/a/58705306
-   getTextWidth(text, font) {
+  // https://stackoverflow.com/a/58705306
+  // Get width of user text in px.
+  getTextWidth(text, font) {
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
     context.font = font || getComputedStyle(document.body).font;
     return context.measureText(text).width;
   }
 
+  // Set the position of the blinking cursor to a place just past
+  // the user inputted text.
+  // Essentially,
+  // Set the length of the input box to match the exact length of the input.
   setCaretPosition() {
+    // Get the first child of the line which is just some text.
     var textContent = this.line.current.children[0];
+    // Get the width of the text.
     var newInputLength = this.getTextWidth(textContent.value);
-    console.log(newInputLength); // <-- String length
 
-    //console.log(this.command.current.offsetWidth); // <- Width of input box
+    //console.log(newInputLength);  <-- String length
 
+    //console.log(this.command.current.offsetWidth);  <- Width of input box
+
+    // Set the length of the input box to match the exact length of the input.
     this.setState({commandLengthInEM: newInputLength});
-    //textContent.style.width = 24;
-    //console.log("The part above this doesn't work");
   }
 
   // Set the state every time there is a change to the commandline
@@ -49,20 +57,35 @@ class CommandLine extends React.Component {
     this.setCaretPosition();
   }
 
+  // When a user clicks anywhere on the screen.
+  focusCommandLine() {
+    // Check if a user is selecting text on the screen.
+    var selection = window.getSelection();
+
+    if (selection.toString().length === 0) {
+      // User is not selecting text, they just clicked.
+      this.command.current.focus();
+    }
+  }
+
   render() {
-    return (<div className="CommandLine">
+    return (<div className="CommandLine" onClick={this.focusCommandLine}>
       <header className="CommandLineBackground"></header>
       <div className="CommandLineText">
         <div ref={this.line} className="CommandLineHeadLine">
-          aidan@dark.infinityflame.co.uk ¬ /var/www/subdomains/dark ><input autoFocus={true} ref={this.command} style={{ width: this.state.commandLengthInEM, maxWidth: "95vw" }} onChange={(e) => {
-        this.changeOccuring(e)
-      }} className="CommandLineInput" spellCheck="false" type="text"/>
+          aidan@dark.infinityflame.co.uk ¬ /var/www/subdomains/dark >
+          <input autoFocus={true} ref={this.command} style={{
+              width: this.state.commandLengthInEM,
+              maxWidth: "95vw"
+            }} onChange={(e) => {
+              this.changeOccuring(e)
+            }} className="CommandLineInput" spellCheck="false" type="text"/>
           <span className="blink">█</span>
         </div>
       </div>
     </div>
     );
-}
+  }
 }
 
 export default CommandLine;
